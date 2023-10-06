a6ad2ccb9722717b630f242080399b65a47cc66b
Lucrecio24 <lucas-nmo@hotmail.com>
2023-10-06 17:17:49 -0300

    Clippy-ed message_handler

--- src/message_handler.rs
+++ src/message_handler.rs
@@ -9,27 +9,31 @@ use serenity::{
 };
 
 pub async fn run(ctx: serenity::client::Context, msg: Message){
+    // author.id 1004145386887319692 is the bot itself.
+    // Below, we check if the bot himself didn't say the words that trigger the response.
+    // Endless loops could be triggered elseway
 
-    if (msg.channel_id == 1004982773121032242) && (msg.author.id != 1004145386887319692) {
-        _ = msg.delete(&ctx).await;
-        println!("Borré la wea de mensaje");
-    }
-    if msg.content.to_lowercase().contains("toty") && (msg.author.id != 1004145386887319692) {
-        match msg.channel_id.say(&ctx, "El toty se la come").await {
-            Err(error) => println!("Error sending message {:?}", error),
-            _ => {}
+    if msg.author.id != 1004145386887319692 {
+
+        // No idea what this is, referenced channel might not even exist
+        if msg.channel_id == 1004982773121032242 {
+            _ = msg.delete(&ctx).await;
+            println!("Borré la wea de mensaje");
         }
-    }
-    if msg.content.to_lowercase().contains("toti") & (msg.author.id != 1004145386887319692) {
-        match msg.channel_id.say(&ctx.http, "Es con y").await {
-            Err(error) => println!("Error sending message {:?}", error),
-            _ => {}
+        if msg.content.to_lowercase().contains("toty") {
+            if let Err(error) = msg.channel_id.say(&ctx, "El toty se la come").await {
+                println!("Error sending message {:?}", error);
+            }
         }
-    }
-    if msg.content.to_lowercase().contains("dbd"){
-        match msg.react(ctx, ReactionType::from(parse_emoji("<:tired:1036702905530597447>").unwrap())).await {
-            Err(error) => println!("Error sending message {:?}" , error),
-            _ => {}
+        if msg.content.to_lowercase().contains("toti") {
+            if let Err(error) = msg.channel_id.say(&ctx, "Es con y").await {
+                println!("Error sending message {:?}", error);
+            }
+        }
+        if msg.content.to_lowercase().contains("dbd") {
+            if let Err(error) = msg.react(ctx, ReactionType::from(parse_emoji("<:tired:1036702905530597447>").unwrap())).await {
+                println!("Error sending message {:?}" , error)
+            }
         }
     }
 }
