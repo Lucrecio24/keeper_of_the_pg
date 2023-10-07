57f1dac47678983faf8a2e5621ab330b9c7db47d
Lucrecio24 <lucas-nmo@hotmail.com>
2023-10-06 21:08:43 -0300

    Clippy-ed part of bal.rs

--- src/commands/lanascoin/bal.rs
+++ src/commands/lanascoin/bal.rs
@@ -1,7 +1,5 @@
 use keeper_of_the_pg::*;
-use serenity::model::prelude::interaction::application_command::{
-    ApplicationCommandInteraction,
-};
+use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 
 
 pub async fn run(
@@ -19,13 +17,13 @@ pub async fn run(
     .await;
 
     if let Ok(result) = query_result {
-        return CommandResponse{
+        CommandResponse{
             result_string: format!("Tienes {} LanasCoin" , result.lanas_coin),
             ephemeral: true
         }    
     } else {
-        return CommandResponse{
-            result_string: format!("No estas en la base de datos..."),
+        CommandResponse{
+            result_string: "No estas en la base de datos...".to_string(),
             ephemeral: true
         }    
     }
