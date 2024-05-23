20ab52a96b62608a0b18aa9759fa22b08f0e0538
Lucrecio24 <lucas-nmo@hotmail.com>
2024-05-23 02:20:24 -0400

    Added dm to inform Lucrecio of IP changes

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         crate::discord_bot::interaction_handler::run(self , ctx , interaction).await;
     }
-    async fn resume(&self, _ctx: Context, _: serenity::all::ResumedEvent) {
+    async fn resume(&self, ctx: Context, _: serenity::all::ResumedEvent) {
         
         let mut data = self.data.write().await;
 
@@ -45,7 +45,14 @@ impl EventHandler for Handler {
         };
         let Some(saved_ip) = data.get("saved_ip") else {
             data.insert(String::from("saved_ip"), current_ip.to_string());
-            println!("{} is connected!", current_ip);
+            println!("New IP!: {}", current_ip);
+            let Ok(lucrecio_user) = serenity::model::id::UserId::from(228685282185052160).to_user(&ctx).await else {
+                println!("Couldn't get Lucrecio's user");
+                return;
+            };
+            if let Err(x) = lucrecio_user.direct_message(&ctx, serenity::builder::CreateMessage::new().content(format!("IP changed! {}" , current_ip.to_string()))).await{
+                println!("Couldn't send new IP to Lucrecio.\nERORR: {x}");
+            }
             return;
         };
         if &current_ip.to_string() == saved_ip {
