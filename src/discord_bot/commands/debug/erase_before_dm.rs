e4f99a924a69e89cafad3fc2daaab596968ad1b9
Lucrecio24 <lucas-nmo@hotmail.com>
2024-05-26 17:20:10 -0400

    Erased commented code remaining from template

--- src/discord_bot/commands/debug/erase_before_dm.rs
+++ src/discord_bot/commands/debug/erase_before_dm.rs
@@ -76,47 +76,4 @@ pub async fn run(
         result_string: "Mensajes borrados exitosamente".to_string(),
         ephemeral: true
     };
-    /*
-    // Saving command option as target user, or returning if invalid user (No idea if it is possible)
-    let Some(target_user_id) = command.clone().data.options[0].value.as_user_id() else {
-        return CommandResponse{
-            result_string: "Miembro a buscar no ingresado".to_string(),
-            ephemeral: true
-        };
-    };
-    let Ok(member) = command.guild_id.unwrap().member(&ctx, &target_user_id).await else {
-        return CommandResponse{
-            result_string: "No se ha podido conectar con discord".to_string(),
-            ephemeral: true
-        };
-    };
-    let user_id_for_query = target_user_id.clone().to_string();
-    let query_result = sqlx::query!(
-        "SELECT * FROM members WHERE account_id = ?",
-        user_id_for_query
-    ).fetch_all(database).await;
-
-    let Ok(query_result) = query_result else {
-        return CommandResponse{
-            result_string: "User not found in database...".to_string(),
-            ephemeral: true
-        };
-    };
-    let member_nick;
-    if let Some(nick) = member.nick {
-        member_nick = nick;
-    } else {
-        member_nick = "None".to_string();
-    }
-    return CommandResponse{
-        result_string: format!(
-            "Information about {}:\nCurrent nick: {}\nMax rank: {}\nLanasCoins: {}",
-            Mention::from(target_user_id),
-            member_nick,
-            rank_to_string(serenity::model::id::RoleId::from(query_result.get(0).unwrap().rank_id.parse::<u64>().unwrap())),
-            query_result.get(0).unwrap().lanas_coin
-        ),
-        ephemeral: true
-    };
-    */
 }
