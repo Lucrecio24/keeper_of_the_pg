f7d361d83fac6d05174bc3c57e0a979f925d3fae
Lucrecio24 <lucas-nmo@hotmail.com>
2024-07-11 18:34:58 -0400

    Was missing a file last commit, or it didn't update

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ impl EventHandler for Handler {
         crate::discord_bot::new_member_handler::run(ctx , new_member).await;
     }
     async fn guild_audit_log_entry_create(&self , ctx: Context , entry: serenity::model::guild::audit_log::AuditLogEntry , guild_id: GuildId){
-        crate::discord_bot::updated_member_handler::run(ctx , entry , guild_id).await;
+        crate::discord_bot::audit_log_update_handler::run(ctx , entry , guild_id).await;
     }
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         crate::discord_bot::interaction_handler::run(self , ctx , interaction).await;
